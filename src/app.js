7f0d7df7979d0977c5d88409c0c46073a6565f6c
Nyingcho Christian Maluyen <nyingchochris12@gmail.com>
2026-09-23 17:20:07 -0700

    feat

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
-const express = 
-require("express")
+const express = require("express")
+const dotenv = require('dotenv');
+const connectDB = require('./db');
+const taskRoutes = require('./routes/taskRoutes');
+
+dotenv.config();
+connectDB();
+
+
 const app = express();
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
+app.use('/api/Tasks', taskRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`Taskit Up and running sir HTTP://localhost:${PORT}`)

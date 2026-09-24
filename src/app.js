607f3b50867316d48305bde28c135fbd1896a239
Nyingcho Christian Maluyen <nyingchochris12@gmail.com>
2026-09-24 01:43:06 -0700

    feat: create a simple task form feat: integrated frontend with backend fix: corrected the port number in the backend server

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const dotenv = require('dotenv');
+const cors = require('cors');
 const connectDB = require('./db');
 const taskRoutes = require('./routes/taskRoutes');
 
@@ -9,11 +10,13 @@ connectDB();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(express.json())
-app.use(cors());
-app.use('/api/Tasks', taskRoutes);
+
+app.use('/api/tasks', taskRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`Taskit Up and running sir HTTP://localhost:${PORT}`)
